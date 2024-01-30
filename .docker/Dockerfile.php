5e2c94c3d14ad93d32176bd241bd9cd0ee0adbc7
Ezra Lazuardy <ezralucio@gmail.com>
2024-01-31 05:43:31 +0700

    feat: initial commit

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -1,5 +1,6 @@
 FROM php:7.4-fpm-alpine
-RUN apk add --no-cache freetype libpng libjpeg-turbo freetype-dev libpng-dev libjpeg-turbo-dev zlib-dev libzip-dev tidyhtml-dev curl-dev
+RUN apk add --no-cache freetype libpng libjpeg-turbo freetype-dev jpeg-dev libpng-dev libjpeg-turbo-dev zlib-dev libzip-dev tidyhtml-dev curl-dev
+RUN docker-php-ext-configure gd --with-jpeg
 RUN docker-php-ext-install pdo pdo_mysql mysqli zip bcmath gd json tidy curl fileinfo exif
 COPY ./.docker/php.ini /usr/local/etc/php/
 EXPOSE 9000
